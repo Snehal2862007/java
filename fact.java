dd00ae92f7620e1758375abb28ff3343bb18382e
Snehal Ranjeet Dalavi <snehaldalvi9560@gmail.com>
2026-01-09 12:38:05 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,11 @@
+class fact {
+    public static void main(String args[]) {
+int a=2;
+long factorial=1;
+for(int i=1;i<=a;i++)
+{
+factorial*=i;
+}
+        System.out.println(factorial);
+    }
+}
